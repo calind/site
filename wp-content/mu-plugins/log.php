f2b56ba003c7f8d3396f73a7acd7723a3113e1d4
Calin Don <calin.don@gmail.com>
2020-04-28 18:02:06 +0300

    Remove unused mu-plugins

--- wp-content/mu-plugins/log.php
+++
@@ -1,9 +0,0 @@
-<?php
-// add_action( 'init', 'do_sleep' );
-// function do_sleep() {
-// 	global $wpdb;
-// 	if ( defined( 'DOING_CRON' ) && DOING_CRON ) {
-// 		error_log( 'doing cron' );
-// 		$wpdb->query("SELECT SLEEP(3)");
-// 	}
-// }
