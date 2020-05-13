391ed4b960cb1ca7a66a4c55cef7a0c51325b1a1
Presslabs Support <support@presslabs.com>
2020-05-13 13:23:36 +0000

    Removed wflogs

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"pF[yh{f!e1uwgt{*)mK:j@q#u4iz5_9I_FW],o^>sDS52fm>5mbb#~CY+8:OOZ~G";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3508;}
