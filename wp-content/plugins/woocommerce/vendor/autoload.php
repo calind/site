f029fa93fd5afc366c448e8c490c28507c6c6c83
Presslabs Support <support@presslabs.com>
2020-10-07 08:20:58 +0000

    updated plugin `WooCommerce` version 4.5.2

--- wp-content/plugins/woocommerce/vendor/autoload.php
+++ wp-content/plugins/woocommerce/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit6740a6f568066dfec23033b636a5e288::getLoader();
+return ComposerAutoloaderInit1e69adc4e12f3a0de649acee72d963eb::getLoader();
